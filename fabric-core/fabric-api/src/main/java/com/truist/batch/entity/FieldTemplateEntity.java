d298e79ec399447685cf68d0942dec81eef5a13f
Pavan Kanduri <pkanduri1@gmail.com>
2025-11-18 21:26:58 -0500

    fix: add missing transformationType and transformationConfig fields to FieldTemplateEntity and FieldTemplate

--- fabric-core/fabric-api/src/main/java/com/truist/batch/entity/FieldTemplateEntity.java
+++ fabric-core/fabric-api/src/main/java/com/truist/batch/entity/FieldTemplateEntity.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
  * Oracle database entity for cm3int.field_templates table
  */
 public class FieldTemplateEntity {
-    
+
     private String id;
     private String fileType;
     private String transactionType;
@@ -26,62 +26,171 @@ public class FieldTemplateEntity {
     private String defaultValue;
     private String validationRule;
     private Integer version;
-    
+    private String transformationType;
+    private String transformationConfig;
+
     // Default constructor
-    public FieldTemplateEntity() {}
-    
+    public FieldTemplateEntity() {
+    }
+
     // Getters and setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    
-    public String getFileType() { return fileType; }
-    public void setFileType(String fileType) { this.fileType = fileType; }
-    
-    public String getTransactionType() { return transactionType; }
-    public void setTransactionType(String transactionType) { this.transactionType = transactionType; }
-    
-    public String getFieldName() { return fieldName; }
-    public void setFieldName(String fieldName) { this.fieldName = fieldName; }
-    
-    public Integer getTargetPosition() { return targetPosition; }
-    public void setTargetPosition(Integer targetPosition) { this.targetPosition = targetPosition; }
-    
-    public Integer getLength() { return length; }
-    public void setLength(Integer length) { this.length = length; }
-    
-    public String getDataType() { return dataType; }
-    public void setDataType(String dataType) { this.dataType = dataType; }
-    
-    public String getFormat() { return format; }
-    public void setFormat(String format) { this.format = format; }
-    
-    public String getRequired() { return required; }
-    public void setRequired(String required) { this.required = required; }
-    
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    
-    public String getEnabled() { return enabled; }
-    public void setEnabled(String enabled) { this.enabled = enabled; }
-    
-    public String getCreatedBy() { return createdBy; }
-    public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
-    
-    public LocalDateTime getCreatedDate() { return createdDate; }
-    public void setCreatedDate(LocalDateTime createdDate) { this.createdDate = createdDate; }
-    
-    public String getModifiedBy() { return modifiedBy; }
-    public void setModifiedBy(String modifiedBy) { this.modifiedBy = modifiedBy; }
-    
-    public LocalDateTime getModifiedDate() { return modifiedDate; }
-    public void setModifiedDate(LocalDateTime modifiedDate) { this.modifiedDate = modifiedDate; }
-    
-    public String getDefaultValue() { return defaultValue; }
-    public void setDefaultValue(String defaultValue) { this.defaultValue = defaultValue; }
-    
-    public String getValidationRule() { return validationRule; }
-    public void setValidationRule(String validationRule) { this.validationRule = validationRule; }
-    
-    public Integer getVersion() { return version; }
-    public void setVersion(Integer version) { this.version = version; }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public Integer getTargetPosition() {
+        return targetPosition;
+    }
+
+    public void setTargetPosition(Integer targetPosition) {
+        this.targetPosition = targetPosition;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getRequired() {
+        return required;
+    }
+
+    public void setRequired(String required) {
+        this.required = required;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getValidationRule() {
+        return validationRule;
+    }
+
+    public void setValidationRule(String validationRule) {
+        this.validationRule = validationRule;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getTransformationType() {
+        return transformationType;
+    }
+
+    public void setTransformationType(String transformationType) {
+        this.transformationType = transformationType;
+    }
+
+    public String getTransformationConfig() {
+        return transformationConfig;
+    }
+
+    public void setTransformationConfig(String transformationConfig) {
+        this.transformationConfig = transformationConfig;
+    }
 }
