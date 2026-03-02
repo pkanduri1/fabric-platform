d015a29b2d781a4864c98efdc67361c8752405ec
Buddy <buddy@mac.mynetworksettings.com>
2026-03-02 10:57:26 -0500

    chore: remove unused imports from LocalDataSourceConfig

--- fabric-core/fabric-api/src/main/java/com/fabric/batch/config/LocalDataSourceConfig.java
+++ fabric-core/fabric-api/src/main/java/com/fabric/batch/config/LocalDataSourceConfig.java
@@ -2,8 +2,6 @@ package com.fabric.batch.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
